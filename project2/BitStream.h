f1a95f4f41b1eb2a50ab814f1783e90eecff4ba8
gabriel costa <gabriel.costa@ua.pt>
2024-11-18 16:55:17 +0000

    making BitStream a class

--- project2/BitStream.h
+++ project2/BitStream.h
@@ -8,117 +8,131 @@
 
 using namespace std;
 namespace fs = filesystem;
+class BitStream
+{
+    char bit_buffer;
+    int current_bit = 0;
 
-static char bit_buffer;
-static int current_bit=0;
+    int readBitPos = 0;
+    char readBuffer;
+    fstream file;
 
-static int readBitPos=0;
-static char readBuffer;
-static string currentFilename = "";
-static ifstream inputFile;
+public:
+    BitStream(const string &filename, bool readMode){
+        file.open(filename,readMode ? ios::in | ios::binary : ios::out | ios::binary);
+        if (!file.is_open()) {
+            throw runtime_error("Unable to open file");
+        }
+    }
 
-// Writes a single bit to the file.
-void writeBit(string filename, bool bit){
-    bit_buffer <<= 1;
-    bit_buffer |= (bit); // set the last bit to the value of bit
-    // printf("bit: %d,  (binary: %s)\n",bit,bitset<8>(bit_buffer).to_string().c_str());
-    current_bit++;
-    if(current_bit == 8){
-        ofstream file(filename,ios::binary | ios::app);
-        // printf("writing: %c, (binary: %s)\n",bit_buffer,bitset<8>(bit_buffer).to_string().c_str());
-        // file << bit_buffer;
-        file.put(bit_buffer);
-        bit_buffer=0;
-        current_bit = 0;
+    ~BitStream(){
         file.close();
     }
-    
-}
-void flush_Bits (void)
-{
-    while (current_bit)
-        writeBit ("test.bin",0);
-}
-
-// Reads a single bit from the file.
-char readBit(string filename){
-    if(filename != currentFilename ){
-        if(inputFile.is_open()) 
-            inputFile.close();
-        currentFilename = filename;
-        inputFile.open(filename, ios::binary);
-        // printf("HERE\n");
-    }
-    if (readBitPos == 0){
-        inputFile.get(readBuffer);
-        if(inputFile.eof()){
-            //printf("Reached the end of the file\n");
-            return -1;
-        }
-            
-        readBitPos = 8;
-        // printf("here\n");
-    }
-
-    bool bit = (readBuffer & 0x80); // get the most significant bit
-    readBuffer <<= 1;
-    readBitPos--;
-    
-    return bit;
-}
-
-//Writes an integer value represented by N bits to the file, where 0 < N < 64 .
-void writeBits(string filename, uint64_t bits, int N){
-    //static_assert(N>0 && N<64, "N must be between 0 and 64");
-    //static_assert(bits != NULL, "bits must not be NULL");
-    
-    for (int i = N - 1; i >= 0; --i) {
-        bool bit = (bits >> i) & 1; // Extract the i-th bit
-        writeBit(filename, bit);
-        //printf("bit: %d\n",bit);
-    }
-}
-
-// Reads an integer value represented by N bits from the file, where 0 < N < 64 
-// Reads an integer value represented by N bits from the file, where 0 < N <= 64.
-uint64_t readBits(string filename, int N) {
-    assert(N > 0 && N <= 64); // Ensure valid range for N
-
-    uint64_t result = 0; // Initialize result to zero
-
-    for (int i = 0; i < N; ++i) {
-        int bit = readBit(filename);
-        //printf("bit: %d\n",bit);
-        if (bit == -1) {
-            return -1;// End of file
-        }
-        result = (result << 1) | bit; // Shift left and add the current bit
-    }
-    //printf("result: %s\n", bitset<8>(result).to_string().c_str());
-    return result;
-}
-
-
-// Writes a string of characters to the file as a series of bits.
-void writeString(string filename, string str){
-    for (char c : str) {
-        // convert the character to an 8bit unsigned integer
-        writeBits(filename, c, 8);
-    }
-
-}
-
-// Reads a string of characters from the file as a series of bits.
-string readString(string filename){
-    string result;
-    while (true)
+    // Writes a single bit to the file.
+    void writeBit(bool bit)
     {
-        char c = readBits(filename, 8);
-        //printf("C: %c\n",c);
-        if (c == -1) {
-            break;
+        bit_buffer <<= 1;
+        bit_buffer |= (bit); // set the last bit to the value of bit
+        // printf("bit: %d,  (binary: %s)\n",bit,bitset<8>(bit_buffer).to_string().c_str());
+        current_bit++;
+        if (current_bit == 8)
+        {
+            // printf("writing: %c, (binary: %s)\n",bit_buffer,bitset<8>(bit_buffer).to_string().c_str());
+            // file << bit_buffer;
+            file.put(bit_buffer);
+            bit_buffer = 0;
+            current_bit = 0;
         }
-        result += c;
     }
-    return result;
-}
+    // void flush_Bits(void)
+    // {
+    //     while (current_bit)
+    //         writeBit("test.bin", 0);
+    // }
+
+    // Reads a single bit from the file.
+    char readBit()
+    {
+        if (readBitPos == 0)
+        {
+            file.get(readBuffer);
+            if (file.eof())
+            {
+                // printf("Reached the end of the file\n");
+                return -1;
+            }
+
+            readBitPos = 8;
+            // printf("here\n");
+        }
+
+        bool bit = (readBuffer & 0x80); // get the most significant bit
+        readBuffer <<= 1;
+        readBitPos--;
+        // printf("bit: %d\n",bit);
+        return bit;
+    }
+
+    // Writes an integer value represented by N bits to the file, where 0 < N < 64 .
+    void writeBits(uint64_t bits, int N)
+    {
+        // static_assert(N>0 && N<64, "N must be between 0 and 64");
+        // static_assert(bits != NULL, "bits must not be NULL");
+
+        for (int i = N - 1; i >= 0; --i)
+        {
+            bool bit = (bits >> i) & 1; // Extract the i-th bit
+            writeBit(bit);
+            // printf("bit: %d\n",bit);
+        }
+    }
+
+    // Reads an integer value represented by N bits from the file, where 0 < N < 64
+    // Reads an integer value represented by N bits from the file, where 0 < N <= 64.
+    uint64_t readBits(int N)
+    {
+        assert(N > 0 && N <= 64); // Ensure valid range for N
+
+        uint64_t result = 0; // Initialize result to zero
+
+        for (int i = 0; i < N; ++i)
+        {
+            int bit = readBit();
+            // printf("bit: %d\n",bit);
+            if (bit == -1)
+            {
+                return -1; // End of file
+            }
+            result = (result << 1) | bit; // Shift left and add the current bit
+        }
+        // printf("result: %s\n", bitset<8>(result).to_string().c_str());
+        return result;
+    }
+
+    // Writes a string of characters to the file as a series of bits.
+    void writeString(string str)
+    {
+        for (char c : str)
+        {
+            // convert the character to an 8bit unsigned integer
+            writeBits(c, 8);
+        }
+    }
+
+    // Reads a string of characters from the file as a series of bits.
+    string readString()
+    {
+        string result;
+        while (true)
+        {
+            char c = readBits(8);
+            // printf("C: %c\n",c);
+            if (c == -1)
+            {
+                break;
+            }
+            result += c;
+        }
+        return result;
+    }
+};
--- project2/BitStreamTest.cpp
+++ project2/BitStreamTest.cpp
@@ -5,81 +5,93 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    char filename[] = "out.bin";
-    FILE* file = fopen(filename, "wb"); // Open in binary mode for initial clearing
-    fclose(file);  // Close immediately to reset the file
+    // char filename[] = "out.bin";
+    // FILE* file = fopen(filename, "wb"); // Open in binary mode for initial clearing
+    // fclose(file);  // Close immediately to reset the file
     // 72 = H
+    BitStream test1("out.bin",false);
     printf("Testing writeBit\n");
-    writeBit(filename,1);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,1);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,0);
+    test1.writeBit(1);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(1);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(0);
 
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,1);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,0);
-    writeBit(filename,0);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(1);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(0);
+    test1.writeBit(0);
     printf("Passed writeBit\n");
+    test1.~BitStream();
 
+    BitStream test11("out.bin",true);
     printf("Testing readBit\n");
-    assert(readBit(filename) == 1);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 1);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
+    assert(test11.readBit() == 1);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 1);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
 
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 1);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
-    assert(readBit(filename) == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 1);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
+    assert(test11.readBit() == 0);
 
-    assert(readBit(filename) == -1);
+    assert(test11.readBit() == -1);
     printf("Passed readBit\n");
 
 
-    char filename2[] = "out2.bin";
-    file = fopen(filename2, "wb"); // Open in binary mode for initial clearing
-    fclose(file); // Close immediately to reset the file
+    // char filename2[] = "out2.bin";
+    // file = fopen(filename2, "wb"); // Open in binary mode for initial clearing
+    // fclose(file); // Close immediately to reset the file
+
+    BitStream test2("out2.bin",false);
 
     printf("Testing writeBits\n");
-    writeBits(filename2, 0b10101010, 8); 
-    writeBits(filename2, 0b01010101, 8);
-    writeBits(filename2, 0b11110000, 8);
-    writeBits(filename2, 0b00001111, 8);
+    test2.writeBits(0b10101010, 8); 
+    test2.writeBits(0b01010101, 8);
+    test2.writeBits(0b11110000, 8);
+    test2.writeBits(0b00001111, 8);
     printf("Passed writeBits\n");
+    test2.~BitStream();
+
+    BitStream test21("out2.bin",true);
 
     printf("Testing readBits\n");
-    assert(readBits(filename2, 8) == 0b10101010);
-    assert(readBits(filename2, 8) == 0b01010101);
-    assert(readBits(filename2, 8) == 0b11110000);
-    assert(readBits(filename2, 8) == 0b00001111);
+    assert(test21.readBits(8) == 0b10101010);
+    assert(test21.readBits(8) == 0b01010101);
+    assert(test21.readBits(8) == 0b11110000);
+    assert(test21.readBits(8) == 0b00001111);
     printf("Passed readBits\n");
 
-    char filename3[] = "out3.bin";
-    file = fopen(filename3, "wb"); // Open in binary mode for initial clearing
-    fclose(file); // Close immediately to reset the file
+    // char filename3[] = "out3.bin";
+    // file = fopen(filename3, "wb"); // Open in binary mode for initial clearing
+    // fclose(file); // Close immediately to reset the file
 
+
+    BitStream test3("out3.bin",false);
     printf("Testing writeString\n");
-    writeString(filename3, "Hello World\n");
+    test3.writeString("Hello World\n");
     printf("Passed writeString\n");
 
+    test3.~BitStream();
+    BitStream test31("out3.bin",true);
     printf("Testing readString\n");
-    assert(readString(filename3) == "Hello World\n");
+    assert(test31.readString() == "Hello World\n");
     printf("Passed readString\n");
 
 
